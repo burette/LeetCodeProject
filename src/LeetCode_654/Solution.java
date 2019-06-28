eb0894698ec99290c62d407e47105de016e63aa7
lizhiwen01 <lizhiwen01@baidu.com>
2019-06-28 10:01:59 +0800

    2019.6.28

--- src/LeetCode_654/Solution.java
+++ src/LeetCode_654/Solution.java
@@ -7,10 +7,30 @@ import java.util.Collections;
 
 public class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        TreeNode root;
-        int max = Arrays.stream(nums).max().getAsInt();
+        return dfs(0, nums.length - 1, nums);
+    }
 
-        return null;
+    public TreeNode dfs(int left, int right, int[] nums) {
+        if (right < left) {
+            return null;
+        } else {
+            int max = Integer.MIN_VALUE;
+            int idx = -1;
+            for (int i = left; i <= right; i++) {
+                if (nums[i] > max) {
+                    max = nums[i];
+                    idx = i;
+                }
+            }
+            TreeNode node = new TreeNode(max);
+            node.left = dfs(left, idx - 1, nums);
+            node.right = dfs(idx + 1, right, nums);
+            return node;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
     }
 }
 
